396f25fe14bd2b324b0585e2557a537c0b67afa2
vicftz <victorfritz.ici@gmail.com>
2020-04-25 15:50:49 +0200

    initialize project

---
+++ cmake-build-debug/CMakeFiles/encryption_3des.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/encryption_3des.dir/main.cpp.o"
+  "encryption_3des"
+  "encryption_3des.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/encryption_3des.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
