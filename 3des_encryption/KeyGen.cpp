4b1e94b418cf85730b026b4320d5f0bc0206284e
Perciii <qs.sauvage@gmail.com>
2020-06-08 16:54:13 +0200

    KeyGen without test

--- 3des_encryption/KeyGen.cpp
+++ 3des_encryption/KeyGen.cpp
@@ -1,9 +1,16 @@
-//#include "KeyGen.h"
-//
-//KeyGen::KeyGen(SequenceD<64> seqD) {
-//
-//}
-//
-//SequenceD<48>& KeyGen::next() {
-//	return generatedKey;
-//}
+#include "KeyGen.h"
+#include "Permutation.h"
+
+KeyGen::KeyGen(SequenceD<64> seqD) {
+	Permutation<64, 56> permutation;
+	roundNo = 0;
+	seqD56 = permutation(seqD, PC1);
+}
+
+SequenceD<48> KeyGen::next() {
+	int bitsToRotate = LS[roundNo++];
+	seqD56.decalage(bitsToRotate);
+
+	Permutation<56, 48> permutation;
+	return permutation(seqD56, PC2);
+}
