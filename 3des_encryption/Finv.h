ffe378d7b06fa504c8b2d262e24b6c88152f2967
deeplygends <peeren.pro@gmail.com>
2020-06-10 19:58:25 +0200

    Add Finv .h and .cpp

---
+++ 3des_encryption/Finv.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "KeyGen.h"
+#include "S_fonction.h"
+
+class Finv
+{
+	public:
+		Finv(SequenceD<64>);
+		Sequence operator()(Sequence seq);
+		static SequenceD<48> keys[16]; //tableau qui stock les 16 clés;
+		int keyToUse;
+	private :
+		KeyGen keygen_;
+		S_fonction s_fonction_;
+};
+
