8dc4b7dac6ce01da772156767ffcfe696f24918d
Perciii <qs.sauvage@gmail.com>
2020-06-11 19:30:22 +0200

    resharper des.cpp

--- 3des_encryption/DES.h
+++ 3des_encryption/DES.h
@@ -4,8 +4,8 @@
 class des
 {
 public:
-	des(sequence_d<64> key);
-	sequence_d<64> operator()(sequence_d<64> seq_d);
+	des(const sequence_d<64>& key);
+	sequence_d<64> operator()(sequence_d<64> seq_d) const;
 
 private:
 	sequence_d<64> key_;
